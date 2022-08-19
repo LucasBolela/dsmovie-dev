56bbeeaebac138493045ec06da4a2656e04abb88
Daniel Pires <dfpires@gmail.com>
2022-08-18 22:37:14 -0300

    Add files via upload

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-//import {ReactComponent as GithubIcon} from 'assets/img/github.svg' 
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg' 
 import './styles.css'
 
 function Navbar(){
@@ -7,11 +7,12 @@ function Navbar(){
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/dfpires/sdmovie" target="_blank" rel="noreferrer"></a>
-                    <div className="dsmovie-contact-container">
-                        {/* <GithubIcon /> */}
-                        <p className="dsmovie-contact-link">/devsuperior</p>
-                    </div>
+                    <a href="https://github.com/dfpires/sdmovie" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/devsuperior</p>
+                        </div>
+                    </a>
                 </div>
             </nav>
         </header>
